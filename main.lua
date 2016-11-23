716647ad67d6b18b6b7826e65431f0f8a5f2c595
Rob Miracle <rob@coronalabs.com>
2016-11-23 17:38:49 -0500

    Added copyright info

--- main.lua
+++ main.lua
@@ -1,3 +1,10 @@
+--
+-- Copyright (c) 2016 Corona Labs Inc.
+-- Code is MIT licensed and can be reused, see https://www.coronalabs.com/links/code/license
+-- Other assets are licensed by their creators:
+--    Art assets by Kenney: http://kenney.nl/assets
+--    Music and sound effect assets by Eric Matyas: http://www.soundimage.org
+-- 
 
 local composer = require( "composer" )
 
