5730fcf617b222aa93b340aa286909a73bf735e7
ignisdesign <brent@ignisdesign.com>
2016-12-09 11:35:36 -1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -14,22 +14,6 @@ display.setStatusBar( display.HiddenStatusBar )
 -- Seed the random number generator
 math.randomseed( os.time() )
 
--- Include Corona Ads
-local coronaAds = require( "plugin.coronaAds" )
-local apiKey = ""
-
--- Corona Ads listener function
-local function adListener( event )
-
-	-- Successful initialization of Corona Ads
-	if ( event.phase == "init" ) then
-		composer.setVariable( "coronaAdsInitialized", true )
-	end
-end
-
--- Initialize Corona Ads
-coronaAds.init( apiKey, adListener )
-
 -- Reserve channel 1 for background music
 audio.reserveChannels( 1 )
 -- Reduce the overall volume of the channel
