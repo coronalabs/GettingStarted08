728942bf31345b29a4cbd7472f6a6feefdcea1f9
ignisdesign <bjsorrentino@hotmail.com>
2017-02-08 14:29:35 -1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,8 @@
---
--- Copyright (c) 2016 Corona Labs Inc.
--- Code is MIT licensed and can be reused, see https://www.coronalabs.com/links/code/license
+-- Copyright (c) 2017 Corona Labs Inc.
+-- Code is MIT licensed and can be re-used; see https://www.coronalabs.com/links/code/license
 -- Other assets are licensed by their creators:
 --    Art assets by Kenney: http://kenney.nl/assets
 --    Music and sound effect assets by Eric Matyas: http://www.soundimage.org
--- 
 
 local composer = require( "composer" )
 
